7b446db3f0058831473bd09b6493f5adfbde8cbd
sawfiz <dashu.up@gmail.com>
2023-11-17 18:38:08 +0800

    Add passport local strategy log in

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
-const createError = require('http-errors');
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
+const session = require("express-session");
+
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require('bcryptjs')
+
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const cors = require('cors');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const dataRouter = require('./routes/data');
 const apiAthleteRouter = require('./routes/api_athletes');
 const apiUserRouter = require('./routes/api_users');
+
+const User = require('./models/user')
 
 const app = express();
 app.use(cors()); // Enable CORS for all routes
@@ -30,6 +39,56 @@ async function connectToMongoDB() {
 
 // Connect to the database when the app starts
 connectToMongoDB();
+
+// ^Passport related, insert before view set up
+// This function will be called when we use the passport.authenticate() function
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password)
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      };
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+// Function to create a cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Function to decode a cookie
+// Add const User = require('./models/user')
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+// Make sure to add SESSION_SECRET in .env
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Make currentUser globally available including the views
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// ^-----
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'T-Tracker' });
+});
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      return res.status(200).json({ message: 'Logged in successfully', user });
+    });
+  })(req, res, next);
 });
 
 module.exports = router;
